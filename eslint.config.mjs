f7fcc7808049c56da712e43e4b0ad46ed81873d5
User <user@example.com>
2025-08-25 15:42:31 -0400

    asdqw

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals"),
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
